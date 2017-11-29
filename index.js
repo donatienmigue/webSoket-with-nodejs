80ad0551bc7741f3f91876d110d256ec52d9c0a8
migue <miguedonatien1@gmail.com>
2017-11-29 23:59:51 +0200

    all files are fixed

---
+++ index.js
@@ -0,0 +1,31 @@
+let express = require('express');
+let socket = require('socket.io');
+
+//App setup
+
+let app = express();
+
+let Server = app.listen(3000, (req, res) => {
+  console.log("Server running on http://localhost:3000");
+});
+
+// Static file
+
+app.use(express.static('public'));
+
+// socket setup
+
+let io = socket(Server);
+
+io.on('connection',function (socket) {
+  console.log("made socket connection", socket.id);
+  
+  socket.on('chat',function(data) {
+    io.sockets.emit('chat',data);
+  });
+  socket.on('typing',(data) => {
+    socket.broadcast.emit('typing',data);
+  });
+  
+});
+
---
+++ public/chat.js
@@ -0,0 +1,42 @@
+//Make a connection
+
+let socket = io.connect('http://localhost:3000');
+
+// Query DOM
+
+let message = document.getElementById('message');
+    user = document.getElementById('user'),
+    output = document.getElementById('output'),
+    send = document.getElementById('send'),
+    feelback = document.getElementById('feelback');
+    
+//Emit Events
+
+send.addEventListener("click",function() {
+  
+  socket.emit('chat',{
+    
+    user : user.value, 
+    message : message.value
+    
+  });
+  
+});     
+
+message.addEventListener("keypress", function() {
+  
+  socket.emit('typing', user.value);
+  
+});
+
+//Listen for Events
+
+socket.on('chat',function (data) {
+  output.innerHTML += '<p><strong>' +data.user +' :</strong> '+data.message; + '</p>'
+  
+});
+
+socket.on('typing', function(data){
+  feelback.innerHTML = '<p><em>' +data+' is typing a message...</em></p>'
+})
+
