40f5b687085fe3d2e1414b077d8eebe16214c47d
migue donatien <miguedonatien1@gmail.com>
2018-08-27 14:49:28 +0100

    boom

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 let express = require('express');
 let socket = require('socket.io');
+let url = require('url');
 
 //App setup
 
 let app = express();
+let port = process.env.port || 5000;
 
-let Server = app.listen(5000, (req, res) => {
+let Server = app.listen(port, (req, res) => {
   console.log("Server running on http://localhost:5000");
 });
 
@@ -32,5 +34,9 @@ io.on('connection',function (socket) {
 //Routes setup
 
 app.get('/', (req, res) => {
-  res.render(index);
+  const path = url.parse(req.url).pathname.slice(1).replace(/%20/g, ' ')
+    
+    res.writeHead(200, {'Content-Type': 'application/json'})    
+    res.write(index);
+    res.end();
 });
